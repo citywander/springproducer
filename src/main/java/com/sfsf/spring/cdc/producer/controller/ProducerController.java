2042bd0bda333194346bb749d71d5e4d76fff513
forrest.gan <forrest.gan@sap.com>
2019-09-16 17:13:12 +0800

    add sentinal

--- src/main/java/com/sfsf/spring/cdc/producer/controller/ProducerController.java
+++ src/main/java/com/sfsf/spring/cdc/producer/controller/ProducerController.java
@@ -1,19 +1,34 @@
 package com.sfsf.spring.cdc.producer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.sfsf.spring.cdc.producer.Book;
 import com.sfsf.spring.cdc.producer.ErrorMsg;
+import com.sfsf.spring.cdc.producer.svc.ExceptionUtil;
+import com.sfsf.spring.cdc.producer.svc.TestService;
 
 @RestController
 public class ProducerController {
     
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public ErrorMsg getBook() {
+    @Autowired
+    private TestService testService;
+    
+    @RequestMapping(value = "/errors", method = RequestMethod.GET)
+    public ErrorMsg getBooks() {
         return new ErrorMsg("123", "Price too large");
         //return new Book(1L, "Alice", 100);
+    }
+    
+    
+    @SentinelResource(value = "ProducerController#getBooksById", blockHandler = "handleException", blockHandlerClass = {ExceptionUtil.class})
+    @RequestMapping(value = "/books/{bookId}", method = RequestMethod.GET)
+    public Book getBooksById(@PathVariable Integer bookId) {
+        return new Book(1L, "Alice", 100);
     }
 
 }
---
+++ src/main/java/com/sfsf/spring/cdc/producer/svc/TestService.java
@@ -0,0 +1,31 @@
+package com.sfsf.spring.cdc.producer.svc;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
+@Service
+public class TestService {
+    
+    @SentinelResource(value = "test", blockHandler = "handleException", blockHandlerClass = {ExceptionUtil.class})
+    public void test() {
+        System.out.println("Test");
+    }
+
+    @SentinelResource(value = "hello", blockHandler = "exceptionHandler", fallback = "helloFallback")
+    public String hello(long s) {
+        return String.format("Hello at %d", s);
+    }
+    
+    public String helloFallback(long s) {
+        return String.format("Halooooo %d", s);
+    }
+
+    public String exceptionHandler(long s, BlockException ex) {
+        // Do some log here.
+        ex.printStackTrace();
+        return "Oops, error occurred at " + s;
+    }
+
+}
