40463aa00deb59c4f2533f5987aa5be9df25eb4b
i076054 <forrest.gan@sap.com>
2023-03-08 18:24:32 +0800

    add doc

--- src/main/java/com/sfsf/spring/cdc/producer/JpaConfig.java
+++ src/main/java/com/sfsf/spring/cdc/producer/JpaConfig.java
@@ -3,7 +3,10 @@ package com.sfsf.spring.cdc.producer;
 import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -15,8 +18,37 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 public class JpaConfig {
+	
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+	   return new OpenAPI()
+	    .components(new Components().addSecuritySchemes("basicScheme",
+	            new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+	    .info(new Info().title("SpringShop API").version(appVersion)
+	            .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+	
+	@Bean
+	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
+	   return openApi -> openApi.path("/foo",
+	   new PathItem().get(new Operation().operationId("foo").responses(new ApiResponses()
+	   .addApiResponse("default",new ApiResponse().description("")
+	   .content(new Content().addMediaType("fatz", new MediaType()))))));
+	}
 //  
 //  @Autowired
 //  private Environment env;
